24eaf7a3e95884504a0709de9c1d87dc4ef529c5
Suvan <32813609+suvanl@users.noreply.github.com>
2020-02-09 00:36:23 +0000

    GuildSchema: put all settings into dedicated field

--- api/models/Guild.js
+++ api/models/Guild.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const timestamp = require("mongoose-timestamp");
 
 const GuildSchema = new mongoose.Schema({
-    // Guild info
     guildID: {
         type: Number,
         default: 0,
@@ -14,67 +13,69 @@ const GuildSchema = new mongoose.Schema({
         required: true,
         trim: true
     },
-    // Guild settings
-    adminRole: {
-        type: String,
-        default: "Admin",
-        trim: true
-    },
-    autoRoleEnabled: {
-        type: Boolean,
-        default: false,
-        trim: true
-    },
-    autoRoleName: {
-        type: String,
-        default: "Member",
-        trim: true
-    },
-    language: {
-        type: String,
-        default: "en-GB",
-        trim: true
-    },
-    modLogChannel: {
-        type: String,
-        default: "mod-log",
-        trim: true
-    },
-    modLogData: {
-        type: Array,
-        default: ["joinLeave", "nameChange", "kickBan"],
-        trim: true
-    },
-    modLogEnabled: {
-        type: Boolean,
-        default: false,
-        trim: true
-    },
-    modRole: {
-        type: String,
-        default: "Moderator",
-        trim: true
-    },
-    prefix: {
-        type: String,
-        default: "%",
-        trim: true
-    },
-    welcomeChannel: {
-        type: String,
-        default: "general",
-        trim: true
-    },
-    welcomeEnabled: {
-        type: Boolean,
-        default: false,
-        trim: true
-    },
-    welcomeMessage: {
-        type: String,
-        default: "Welcome to {{server}}, {{user}}!",
-        trim: true
+    settings: {
+        adminRole: {
+            type: String,
+            default: "Admin",
+            trim: true
+        },
+        autoRoleEnabled: {
+            type: Boolean,
+            default: false,
+            trim: true
+        },
+        autoRoleName: {
+            type: String,
+            default: "Member",
+            trim: true
+        },
+        language: {
+            type: String,
+            default: "en-GB",
+            trim: true
+        },
+        modLogChannel: {
+            type: String,
+            default: "mod-log",
+            trim: true
+        },
+        modLogData: {
+            type: Array,
+            default: ["joinLeave", "nameChange", "kickBan"],
+            trim: true
+        },
+        modLogEnabled: {
+            type: Boolean,
+            default: false,
+            trim: true
+        },
+        modRole: {
+            type: String,
+            default: "Moderator",
+            trim: true
+        },
+        prefix: {
+            type: String,
+            default: "%",
+            trim: true
+        },
+        welcomeChannel: {
+            type: String,
+            default: "general",
+            trim: true
+        },
+        welcomeEnabled: {
+            type: Boolean,
+            default: false,
+            trim: true
+        },
+        welcomeMessage: {
+            type: String,
+            default: "Welcome to {{server}}, {{user}}!",
+            trim: true
+        }
     }
+    
 });
 
 GuildSchema.plugin(timestamp);
