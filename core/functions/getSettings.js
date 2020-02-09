0ffafac3830cf8544182b7bb21f42320299acc6f
Suvan <32813609+suvanl@users.noreply.github.com>
2020-02-09 01:27:21 +0000

    create getSettings function

--- core/functions/getSettings.js
+++ core/functions/getSettings.js
@@ -1,3 +1,9 @@
+const fetch = require("node-fetch");
+const { BASE_URL } = process.env;
+
 module.exports = client => {
-    //
+    client.getSettings = async (guild) => {
+        const { body } = await fetch(`${BASE_URL}/guilds/${guild.id || "0"}`);
+        return body;
+    };
 };
