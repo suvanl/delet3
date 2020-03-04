69cf716f9bb140323c1780d7555a93bf42261845
Suvan <32813609+suvanl@users.noreply.github.com>
2020-03-04 18:58:01 +0000

    Add database operations (guildCreate/guildDelete)

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,5 +1,29 @@
 const { blue, cyan } = require("chalk");
+const fetch = require("node-fetch");
 
-module.exports = (client, guild) => {
+module.exports = async (client, guild) => {
+    // Store guild data with default settings in database
+    const secret = await client.genSecret();
+
+    const url = `${process.env.URL}/guilds`;
+    const body = { "guildID": parseInt(guild.id), name: guild.name };
+    const meta = { "Content-Type": "application/json", "Authorization": `jwt ${secret.token}` };
+
+    try {
+        await fetch(url, {
+            method: "post",
+            body: JSON.stringify(body),
+            headers: meta
+        });
+
+        // TODO: prevent new guild data being added if guild ID already exists in database (this shouldn't happen in production anyway)
+        // TODO: save data of all guild members (to be used in points/trivia points systems)
+        // TODO: find a way to save new guild data if the bot was added to a guild whilst offline (maybe run this same logic on the message event, if guildID does not exist in db)
+        // TODO: maybe check for errors based on response status code? e.g. this POST request should respond with a 201 code
+    } catch (err) {
+        return client.logger.err(`Error saving new guild data:\n${err.stack}`);
+    }
+
+    // Log guild name/ID and owner name/ID
     client.logger.inf(`${blue("guildCreate")}: "${cyan(guild.name)}" (${guild.id}) | owner: ${cyan(guild.owner.user.tag)} (${guild.owner.user.id})`);
 };
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,7 +1,20 @@
 const { blue, cyan } = require("chalk");
+const fetch = require("node-fetch");
 
-module.exports = (client, guild) => {
+module.exports = async (client, guild) => {
+    // Delete guild data from database
+    const secret = await client.genSecret();
+    const url = `${process.env.URL}/guilds/${guild.id}`;
+    
+    try {
+        await fetch(url, {
+            method: "delete",
+            headers: { "Authorization": `jwt ${secret.token}` }
+        });
+    } catch (err) {
+        return client.logger.err(`Error deleting guild data:\n${err.stack}`);
+    }
+
+    // Log name/ID of guild
     client.logger.inf(`${blue("guildDelete")}: "${cyan(guild.name)}" (${guild.id})`);
-
-    // todo: release guild data from cache
 };
