f7d0d3a2d4426a650593e42956e24fa370c9c9b7
Suvan <32813609+suvanl@users.noreply.github.com>
2020-03-13 13:29:57 +0000

    Update guildCreate event

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,9 +2,12 @@ const { blue, cyan } = require("chalk");
 const fetch = require("node-fetch");
 
 module.exports = async (client, guild) => {
-    // Store guild data with default settings in database
     const secret = await client.genSecret();
 
+    // Prevent new guild data being added if the guild already exists in database (this shouldn't happen in production anyway)
+    if (client.getSettings(guild)) return client.logger.wrn(`${guild.name} already exists in database`);
+
+    // Store guild data with default settings in database
     const url = `${process.env.URL}/guilds`;
     const body = { "guildID": parseInt(guild.id), name: guild.name };
     const meta = { "Content-Type": "application/json", "Authorization": `jwt ${secret.token}` };
@@ -16,8 +19,7 @@ module.exports = async (client, guild) => {
             headers: meta
         });
 
-        // TODO: prevent new guild data being added if guild ID already exists in database (this shouldn't happen in production anyway)
-        // TODO: save data of all guild members (to be used in points/trivia points systems)
+        // TODO: save data of all guild members (to be used in points/trivia points systems) [maybe do this in the trivia command itself]
         // TODO: find a way to save new guild data if the bot was added to a guild whilst offline (maybe run this same logic on the message event, if guildID does not exist in db)
         // TODO: maybe check for errors based on response status code? e.g. this POST request should respond with a 201 code
     } catch (err) {
