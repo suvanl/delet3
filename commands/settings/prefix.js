3dc368704e2c2c58b2b25beb81cc07f28f7b6279
Suvan <32813609+suvanl@users.noreply.github.com>
2020-03-24 22:04:25 +0000

    Minor text fix (prefix command)

--- commands/settings/prefix.js
+++ commands/settings/prefix.js
@@ -39,7 +39,7 @@ exports.config = {
 
 exports.help = {
     name: "prefix",
-    description: "changes delet's prefix on the current server.",
+    description: "changes delet's prefix on the current server",
     category: "settings",
     usage: "prefix [prefix]"
 };
