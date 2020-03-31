4af92b7ff675524fe8a624bf2d4b86f4601fa062
Suvan <32813609+suvanl@users.noreply.github.com>
2020-03-31 22:54:36 +0100

    massive sigh

--- core/functions/misc.js
+++ core/functions/misc.js
@@ -7,20 +7,3 @@ String.prototype.toTitleCase = function() {
 Array.prototype.random = function() {
     return this[Math.floor(Math.random() * this.length)];
 };
-
-String.prototype.decodeEntities = function() {
-    const translateRe = /&(nbsp|amp|quot|lt|gt);/g;
-    const translate = {
-        "nbsp": " ",
-        "amp": "&",
-        "quot": "\"",
-        "lt": "<",
-        "gt": ">"
-    };
-    return this.replace(translateRe, (match, entity) => {
-        return translate[entity];
-    }).replace(/&#(\d+);/gi, (match, numStr) => {
-        const num = parseInt(numStr, 10);
-        return String.fromCharCode(num);
-    });
-};
