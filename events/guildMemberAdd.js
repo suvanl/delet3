520614d5224b03a02a4ecd7c6f799e768f81c4b2
Suvan <32813609+suvanl@users.noreply.github.com>
2020-04-03 01:14:18 +0100

    Save new guildMembers to DB (guildMemberAdd event)

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,26 @@
-module.exports = (client, member) => {
+const fetch = require("node-fetch");
+
+module.exports = async (client, member) => {
+    // Save new guildMember to DB
+    const secret = await client.genSecret();
+
+    const url = `${process.env.URL}/users`;
+    const body = { "userID": member.user.id, "guildID": member.guild.id };
+    const meta = { "Content-Type": "application/json", "Authorization": `jwt ${secret.token}` };
+
+    try {
+        await fetch(url, {
+            method: "post",
+            body: JSON.stringify(body),
+            headers: meta
+        });
+    } catch (err) {
+        return client.logger.err(`Error saving new guildMember:\n${err.stack}`);
+    }
+
+    // Welcome message stuff
     const settings = client.getSettings(member.guild);
-
     if (settings.welcomeEnabled !== "true") return;
-
-    // TODO: save new member to DB
 
     // TODO: test this (by setting welcomeEnabled to true in settings)
     const welcomeStr = settings.welcomeMessage;
