8a3b784e71a0beeff01a2643e6adebca315a1b1c
Suvan <32813609+suvanl@users.noreply.github.com>
2020-04-04 16:05:06 +0100

    Update jest.config,js

--- jest.config.js
+++ jest.config.js
@@ -132,7 +132,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "node",
+  testEnvironment: "node", // eslint-disable-line comma-dangle
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
