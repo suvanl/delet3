2d1a9da3d8998342fc2a7eb86f09a58c7be18a85
Suvan <32813609+suvanl@users.noreply.github.com>
2020-04-07 15:26:10 +0100

    Implement deletion of userdata (guildMemberRemove)

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -3,14 +3,25 @@ const fetch = require("node-fetch");
 module.exports = async (client, member) => {
     // Delete guildMember's data from DB
     const secret = await client.genSecret();
-    const url = `${process.env.URL}/users/${member.user.id}`;
+    const meta = { "Authorization": `jwt ${secret.token}` };
+    
+    // Fetch all users
+    const all = await client.getUsers();
 
+    // Filter out users who aren't in this guild
+    const f = all.filter(a => a.guildID === member.guild.id);
+    
+    // Find guildMember by user ID
+    const user = f.filter(u => u.userID === member.user.id);
+
+    // Delete this guildMember from DB (using their unique ID)
+    const url = `${process.env.URL}/users/${user[0]._id}`;
     try {
         await fetch(url, {
             method: "delete",
-            headers: { "Authorization": `jwt ${secret.token}` }
+            headers: meta
         });
     } catch (err) {
-        return client.logger.err(`Error deleting guildMember:\n${err.stack}`);
+        return client.logger.err(`Error deleting a user's data (guildMemberRemove)\n${err.stack}`);
     }
 };
