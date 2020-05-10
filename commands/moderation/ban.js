317a0dc2250b2a30256cead2cb41f1af7a08c9aa
Suvan <32813609+suvanl@users.noreply.github.com>
2020-05-10 14:14:39 +0100

    Localise ban command

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -3,9 +3,12 @@ const { stripIndents } = require("common-tags");
 
 exports.run = async (client, message, args) => {
     // Only run if message author has the BAN_MEMBERS permission
+    // and inform user if they do not have the required perm:
+        // 🚫 Insufficient permissions
+        // You must have the "Ban Members" permission to use this command.
     if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(stripIndents`
-        🚫 **Insufficient permissions**
-        You must have the "Ban Members" permission to use this command.`);
+        🚫 **${client.l10n(message, "perm.insufficient")}**
+        ${client.l10n(message, "perm.insufficient.info").replace(/%perm%/g, client.l10n(message, "perm.banMembers"))}`);
 
     // If modLogEnabled is true, check if modLogChannel exists
     // This check should only fail if modLogChannel is left as default,
@@ -13,16 +16,21 @@ exports.run = async (client, message, args) => {
     let modLog;
     if (message.settings.modLogEnabled) {
         modLog = message.guild.channels.cache.find(c => c.name === message.settings.modLogChannel);
+        // ⚠ Mod-log channel not found
+        // Please use %modlog to set it to a channel that exists on this server.
         if (!modLog) return message.channel.send(stripIndents`
-            ⚠️ **Mod-log channel not found**
-            Please use \`${message.settings.prefix}modlog\` to set it to a channel that exists on this server.`);
+            ⚠️ **${client.l10n(message, "mod.noModLog")}**
+            ${client.l10n(message, "mod.noModLog.info").replace(/%cmd%/g, `${message.settings.prefix}modlog`)}`);
     }
 
-    // Inform user if no args are specified and direct them towards help
+    // Inform user if no args are specified and direct them towards help:
+        // ℹ User not specified
+        // You can specify a user by mentioning them, or by providing their user ID.
+        // Use %help ban for further details.
     if (!args.length) return message.channel.send(stripIndents`
-        ℹ **User not specified**
-        You can specify a user by mentioning them, or by providing their user ID.
-        Use \`${message.settings.prefix}help ban\` for further details.`);
+        ℹ **${client.l10n(message, "mod.noUser")}**
+        ${client.l10n(message, "mod.noUser.info")}
+        ${client.l10n(message, "details").replace(/%cmd%/g, `${message.settings.prefix}help ban`)}`);
 
     // Guild members cache
     const cache = message.guild.members.cache;
@@ -33,14 +41,14 @@ exports.run = async (client, message, args) => {
     // Define user: can be @mention or user ID
     const user = args[0].match(re) ? message.mentions.users.first() : (cache.find(m => m.id === args[0]) ? cache.find(m => m.id === args[0]).user : false);
     if (!user) return message.channel.send(stripIndents`
-        ℹ **User not found**
-        Please ensure you've mentioned a user in this server or provided a valid user ID.`);
+        ℹ **${client.l10n(message, "mod.invalidUser")}**
+        ${client.l10n(message, "mod.invalidUser.info")}`);
 
     // Get user's GuildMember object
     const member = message.guild.member(user);
 
     // Check if member is bannable
-    if (!member.bannable) return message.channel.send("The specified user cannot be banned.");
+    if (!member.bannable) return message.channel.send(client.l10n(message, "mod.ban.notBannable"));
 
     // Days of message history to delete
     let days = parseInt(args[1]);
@@ -50,17 +58,20 @@ exports.run = async (client, message, args) => {
 
     // If a reason isn't provided...
     if (!reason) {
-        // Prompt for reason
+        // Prompt for reason:
+            // ℹ You're about to ban %user% without specifying a reason.
+            // • Reply with `continue` to proceed, or with `cancel to exit`
+            // • Alternatively, to provide a reason, simply enter it below:
         const msg = stripIndents`
-            ℹ You're about to ban **${user.tag}** without specifying a reason.
+            ℹ ${client.l10n(message, "mod.ban.noReason").replace(/%user%/g, `**${user.tag}**`)}
 
-            • Reply with \`continue\` to proceed, or with \`cancel\` to exit.
-            • Alternatively, to provide a reason, simply enter it below:`;
+            • ${client.l10n(message, "prompt.continue")}
+            • ${client.l10n(message, "prompt.enterReason")}`;
 
         reason = await client.awaitReply(message, msg);
 
         // Return if 60s is up, or if user replies with "cancel"
-        if (!reason || reason.toLowerCase() === "cancel") return message.channel.send("🚪 Ended the ban procedure.");
+        if (!reason || reason.toLowerCase() === "cancel") return message.channel.send(`🚪 ${client.l10n(message, "mod.cancel")}`);
 
         // Set reason to null if user wishes to proceed without setting a reason
         if (reason.toLowerCase() === "continue") reason = null;
@@ -78,7 +89,7 @@ exports.run = async (client, message, args) => {
     try {        
         const ban = await member.ban({ days, reason });
         await client.updateCaseNumber(message.guild);
-        message.channel.send(`🔨 **${ban.user.tag}** has been banned from the server.`);
+        message.channel.send(`🔨 ${client.l10n(message, "mod.ban.inform").replace(/%user%/g, `**${ban.user.tag}**`)}`);
     } catch (err) {
         message.channel.send(`<:x_:688400118327672843> ${client.l10n(message, "error")}`);
         return client.logger.err(`error banning a user:\n${err.stack}`);
@@ -95,16 +106,24 @@ exports.run = async (client, message, args) => {
         // though it might be harder to fetch the reason;
         // refer to audit log for reason info?
 
-        // Create and send embed
+        // Embed footer data
+        const issuedBy = client.l10n(message, "mod.embed.issued").replace(/%user%/g, message.author.tag);
+        const caseNumber = client.l10n(message, "mod.embed.case").replace(/%num%/g, caseNum);
+
+        // Create and send embed (to modLogChannel):
+            // 🔨 Action: Ban
+            // 👤 Member: %user%
+            // ❔ Reason: %rsn%
+            // Issued by %user% | Case %num%
         const embed = new MessageEmbed()
             .setColor("#f04747")
             .setThumbnail(user.displayAvatarURL())
             .setDescription(stripIndents`
-                🔨 Action: **Ban**
+                🔨 ${client.l10n(message, "mod.embed.action").replace(/%act%/g, `**${client.l10n(message, "ban.noun")}**`)}
 
-                👤 Member: **${user.tag}**
-                ❔ Reason: **${reason ? reason : "No reason provided"}**`)
-            .setFooter(`Issued by ${message.author.tag} • Case ${caseNum}`, message.author.displayAvatarURL());
+                👤 ${client.l10n(message, "mod.embed.member").replace(/%user%/g, `**${user.tag}**`)}
+                ❔ ${client.l10n(message, "mod.embed.reason").replace(/%rsn%/g, `**${reason ? reason : client.l10n(message, "mod.ban.reason.null")}**`)}`)
+            .setFooter(`${issuedBy} • ${caseNumber}`, message.author.displayAvatarURL());
 
         return modLog.send(embed);
     }
