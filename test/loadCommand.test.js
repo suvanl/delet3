266506ff1ae1e37812f757e7a0742e4c58284f9b
suvanl <32813609+suvanl@users.noreply.github.com>
2021-04-11 14:37:32 +0100

    Move test directory out of core

--- test/loadCommand.test.js
+++ test/loadCommand.test.js
@@ -1,4 +1,4 @@
-const loadCommand = require("../functions/loadCommand");
+const loadCommand = require("../core/functions/loadCommand");
 
 test("loadCommand returns the client", () => {
     const lc = loadCommand("../../commands/info", "ping.js");
