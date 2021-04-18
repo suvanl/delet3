caadc4905f9ecfa4f9cc3d93ce8046b28c261f90
suvanl <32813609+suvanl@users.noreply.github.com>
2021-04-18 16:34:59 +0100

    Add comments to awaitReply function

--- core/functions/awaitReply.js
+++ core/functions/awaitReply.js
@@ -1,9 +1,16 @@
 module.exports = client => {
     client.awaitReply = async (msg, question, limit = 60000) =>  {
+        // await messages from the message author only
         const filter = m => m.author.id === msg.author.id;
+
+        // send the prompt/question message
         await msg.channel.send(question);
+
         try {
+            // call awaitMessages with a maximum of 1 message to collect, which times out after the specified time limit (60 sec by default)
             const collected = await msg.channel.awaitMessages(filter, { max: 1, time: limit, errors: ["time"] });
+
+            // return the contents of the user's response
             return collected.first().content;
         } catch (err) {
             return false;
