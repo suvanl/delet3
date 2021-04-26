f78856cd603987da83e51081f65a1675a53bf620
suvanl <32813609+suvanl@users.noreply.github.com>
2021-04-26 22:51:51 +0100

    Place highest role at start of list (user command)

--- commands/info/user.js
+++ commands/info/user.js
@@ -35,12 +35,11 @@ exports.run = async (client, message) => {
         "WATCHING": client.l10n(message, "user.activity.watching")
     };
 
-    console.log(user.presence.activities[0]);
+    // Get the user's current activity
     const currentActivity = user.presence.activities[0];
-    
 
     // If user has a current activity
-    if (user.presence.activities.length !== 0) {
+    if (currentActivity) {
         if (currentActivity.type === "CUSTOM_STATUS") {
             // display their activity in the following format: [Emoji (if present)] [Status Message (if present)]
             activity = `${currentActivity.emoji ? currentActivity.emoji : "\u200b"} **${currentActivity.state ? currentActivity.state.truncate(24) : currentActivity.name}**`;
@@ -53,11 +52,23 @@ exports.run = async (client, message) => {
     // User data for points
     const data = await client.getUser(message.guild, user);
 
-    // Roles (with @everyone filtered out)
-    // TODO: place the user's hoisted (main) role at the start of the list
+    // ROLES:
+    // Get the user's roles (as a collection containing a Map per role)
     const userRoles = message.guild.member(user).roles.cache;
-    const roleMap = userRoles.map(r => `\`${r.name}\``);
-    let roles = roleMap.filter(r => r !== "`@everyone`");
+    
+    // Surround every role's name with backticks, resulting in single-line code formatting for each one
+    const formattedRoles = userRoles.map(r => `\`${r.name}\``);
+
+    // Remove @everyone from the array of roles
+    let roles = formattedRoles.filter(r => r !== "`@everyone`");
+
+    // Get the user's highest role
+    const highestRole = message.guild.member(user).roles.highest.name;
+
+    // Move the user's highest role to the start of the array of roles
+    roles = moveElementToStart(roles, `\`${highestRole}\``);
+
+    // If the user has no roles, display "None"
     if (roles.length === 0) roles = ["None"];
 
     // Create and send embed
@@ -81,6 +92,18 @@ exports.run = async (client, message) => {
         .setFooter(`${client.l10n(message, "user.id")}: ${user.id} | ${client.l10n(message, "utc")}`);
     
     message.channel.send(embed);
+};
+
+// Function to move an element to the start of an array
+moveElementToStart = (array, element) => {
+    // Remove the element from the array
+    array = array.filter(f => f !== element);
+
+    // Use Array.prototype.unshift() to insert the element at the start of the array
+    array.unshift(element);
+
+    // Return the modified array
+    return array;
 };
 
 exports.config = {
