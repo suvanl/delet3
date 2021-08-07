9356048c096b5ea87ae15d6cef294c6148ea4bc9
suvanl <32813609+suvanl@users.noreply.github.com>
2021-08-08 00:02:15 +0100

    user command: Discord.js v13 updates - replace `guild.member(user)` with `guild.members.cache.get(user.id)`
    - send embeds using new format (using an object in the .send() method)

--- commands/info/user.js
+++ commands/info/user.js
@@ -5,18 +5,20 @@ const { badge, statusIcon } = require("../../core/util/data");
 
 exports.run = async (client, message, args) => {
     // Set user as message author by default
-    let user = message.author;
+    let member = message.member;
+
+    const user = client.users.cache.get(member.id);
 
     // If message contains any mentioned users, set the user as the first mention
-    if (message.mentions.users.first()) user = message.mentions.users.first();
+    if (message.mentions.users.first()) member = message.mentions.users.first();
     
     // If args have been supplied but no mentions are present, we'll assume the user is trying to reference a user by
     // their username. Therefore, we'll attempt to find a user in this server that has a username matching the value
     // of the first provided argument.
-    if (args[0] && !message.mentions.users.first()) user = client.users.cache.find(user => user.username == args[0]);
+    if (args[0] && !message.mentions.users.first()) member = message.guild.members.cache.find(user => user.username == args[0]);
 
     // If a user with the specified username cannot be found in the current server, inform the user of this.
-    if (!user) return message.channel.send(client.l10n(message, "avatar.user.invalid"));
+    if (!member) return message.channel.send(client.l10n(message, "avatar.user.invalid"));
 
     
     // Fetch user flags and convert them to an array of bitfield names
@@ -26,7 +28,7 @@ exports.run = async (client, message, args) => {
     // Convert user flags to emojis corresponding to profile badges. These will be displayed on the profile embed.
     let badges = userFlags.map(f => `<:${f.toLowerCase()}:${badge[f.toLowerCase()]}>`);
     // If the user is a bot, display the appropriate "Bot" badge
-    if (badges.length === 0 && user.bot) badges = "<:bot:703336283577122826>";
+    if (badges.length === 0 && member.bot) badges = "<:bot:703336283577122826>";
 
     // Translate status names to the current server's language
     const status = {
@@ -37,7 +39,7 @@ exports.run = async (client, message, args) => {
     };
 
     // Define the status emoji that will be shown in the profile embed (in the format <:emojiName:emojiID>)
-    const statusEmoji = `<:${user.presence.status}:${statusIcon[user.presence.status]}>`;
+    const statusEmoji = `<:${member.presence.status}:${statusIcon[member.presence.status]}>`;
 
     // Activity
     let activity = "";
@@ -52,7 +54,7 @@ exports.run = async (client, message, args) => {
     };
 
     // Get the user's current activity
-    const currentActivity = user.presence.activities[0];
+    const currentActivity = member.presence.activities[0];
 
     // If user has a current activity
     if (currentActivity) {
@@ -66,11 +68,11 @@ exports.run = async (client, message, args) => {
     }
 
     // User data for points
-    const data = await client.getUser(message.guild, user);
+    const data = await client.getUser(message.guild, member);
 
     // ROLES:
     // Get the user's roles (as a collection containing a Map per role)
-    const userRoles = message.guild.member(user).roles.cache;
+    const userRoles = message.guild.members.cache.get(member.id).roles.cache;
     
     // Surround every role's name with backticks, resulting in single-line code formatting for each one
     const formattedRoles = userRoles.map(r => `\`${r.name}\``);
@@ -79,7 +81,7 @@ exports.run = async (client, message, args) => {
     let roles = formattedRoles.filter(r => r !== "`@everyone`");
 
     // Get the user's highest role
-    const highestRole = message.guild.member(user).roles.highest.name;
+    const highestRole = message.guild.members.cache.get(member.id).roles.highest.name;
 
     // Move the user's highest role to the start of the array of roles
     roles = moveElementToStart(roles, `\`${highestRole}\``);
@@ -93,22 +95,22 @@ exports.run = async (client, message, args) => {
         .setColor("#8cfed9")
         .setThumbnail(user.displayAvatarURL({ size: 1024 }))
         .setDescription(stripIndents`
-            **${user.tag}** ${badges} | ${statusEmoji} ${user.presence.activities.length === 0 ? status[user.presence.status].toTitleCase() : activity}
+            **${user.tag}** ${badges} | ${statusEmoji} ${member.presence.activities.length === 0 ? status[member.presence.status].toTitleCase() : activity}
 
             💥 **${client.l10n(message, "user.created")}**
-            ${utc(user.createdTimestamp).format(`DD/MM/YYYY [${client.l10n(message, "user.time.at")}] HH:mm`)}
+            ${utc(member.createdTimestamp).format(`DD/MM/YYYY [${client.l10n(message, "user.time.at")}] HH:mm`)}
 
             🏠 **${client.l10n(message, "user.joined")}**
-            ${utc(message.guild.member(user).joinedTimestamp).format(`DD/MM/YYYY [${client.l10n(message, "user.time.at")}] HH:mm`)}
+            ${utc(message.guild.members.cache.get(member.id).joinedTimestamp).format(`DD/MM/YYYY [${client.l10n(message, "user.time.at")}] HH:mm`)}
 
             🧮 **${client.l10n(message, "points").toTitleCase()}**
             Trivia: ${data.triviaPoints} • ${client.l10n(message, "user.points.regular")}: ${data.points}
 
             📋 **${client.l10n(message, "user.roles")}**
             ${roles.slice(0, 3).join(", ")} ${roles.length >= 4 ? client.l10n(message, "user.roles.more").replace(/%num%/g, roles.length - 3) : ""}`)
-        .setFooter(`${client.l10n(message, "user.id")}: ${user.id} | ${client.l10n(message, "utc")}`);
+        .setFooter(`${client.l10n(message, "user.id")}: ${member.id} | ${client.l10n(message, "utc")}`);
     
-    message.channel.send(embed);
+    message.channel.send({ embeds: [embed] });
 };
 
 // Function to move an element to the start of an array
