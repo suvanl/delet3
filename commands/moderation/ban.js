0d5cf1d3586d11420eaf29534d5e5362679a39f0
suvanl <32813609+suvanl@users.noreply.github.com>
2021-08-08 23:06:18 +0100

    Ban + Unban: ensure embed is sent to modLogChannel

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -46,7 +46,7 @@ exports.run = async (client, message, args) => {
         ${client.l10n(message, "mod.invalidUser.info")}`);
 
     // Get user's GuildMember object
-    const member = message.guild.member(user);
+    const member = message.guild.members.cache.get(user.id);
 
     // Check if member is bannable
     if (!member.bannable) return message.channel.send(client.l10n(message, "mod.ban.notBannable"));
@@ -246,7 +246,7 @@ exports.run = async (client, message, args) => {
                 ❔ ${client.l10n(message, "mod.embed.reason").replace(/%rsn%/g, `**${reason ? reason : client.l10n(message, "mod.ban.reason.null")}**`)}`)
             .setFooter(`${issuedBy} • ${caseNumber}`, message.author.displayAvatarURL());
 
-        return message.channel.send({ embeds: [embed] });
+        return modLog.send({ embeds: [embed] });
     }
 };
 
