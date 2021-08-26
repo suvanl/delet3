0157cdaba0c0c2929e63649ce43fe1eaefa33199
suvanl <32813609+suvanl@users.noreply.github.com>
2021-08-26 14:04:01 +0100

    Create "about" ApplicationCommand

---
+++ interactions/commands/about.js
@@ -0,0 +1,13 @@
+exports.run = async (client, interaction) => {
+    // Get the "about" command from the client.commands collection and call its run() function
+    client.commands.get("about").run(client, interaction);
+};
+
+exports.data = {
+    name: "about",
+    description: "Sends info about myself",
+    options: [],
+    defaultPermission: true
+};
+
+exports.global = false;
