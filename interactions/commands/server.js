5a66c701a337666b59f6a19d6d8198bbd4738151
suvanl <32813609+suvanl@users.noreply.github.com>
2022-01-28 14:42:14 +0000

    Create "server" ApplicationCommand

---
+++ interactions/commands/server.js
@@ -0,0 +1,13 @@
+exports.run = async (client, interaction) => {
+    // Get the "server" command from the client.commands collection and call its run() function
+    await client.commands.get("server").run(client, interaction);
+};
+
+exports.data = {
+    name: "server",
+    description: "Sends info about the current server",
+    options: [],
+    defaultPermission: true
+};
+
+exports.global = false;
