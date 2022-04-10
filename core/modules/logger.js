756a1131b022533aba6c17cc002656cf10626bdb
suvanl <32813609+suvanl@users.noreply.github.com>
2022-04-11 00:24:28 +0100

    logger: CJS -> ESM

--- core/modules/logger.js
+++ core/modules/logger.js
@@ -1,43 +1,43 @@
-const { bgBlue, bgRed, bgYellow, blue, green, grey, magenta, yellow } = require("chalk");
-const moment = require("moment");
+import chalk from "chalk";
+import moment from "moment";
 
-exports.log = (content, type = "log") => {
+export const log = (content, type = "log") => {
     const z = moment().format("Z");
     const tz = !z.includes(":30") ? z.replace(/0|:/g, "") : z;
     const timestamp = `${moment().format("YYYY-MM-DD HH:mm:ss")} ${z === "+00:00" ? "UTC" : `UTC${tz}`} |`;
 
     switch (type) {
         case "cmd": {
-            return console.log(`${timestamp} ${blue(type.toUpperCase())} » ${content}`);
+            return console.log(`${timestamp} ${chalk.blue(type.toUpperCase())} » ${content}`);
         }
         case "app": {
-            return console.log(`${timestamp} ${magenta(type.toUpperCase())} » ${content}`);
+            return console.log(`${timestamp} ${chalk.magenta(type.toUpperCase())} » ${content}`);
         }
         case "dbg": {
-            return console.log(`${timestamp} ${yellow(type.toUpperCase())} » ${content}`);
+            return console.log(`${timestamp} ${chalk.yellow(type.toUpperCase())} » ${content}`);
         }
         case "err": {
-            return console.log(`${timestamp} ${bgRed(type.toUpperCase())} » ${content}`);
+            return console.log(`${timestamp} ${chalk.bgRed(type.toUpperCase())} » ${content}`);
         }
         case "inf": {
-            return console.log(`${timestamp} ${bgBlue(type.toUpperCase())} » ${content}`);
+            return console.log(`${timestamp} ${chalk.bgBlue(type.toUpperCase())} » ${content}`);
         }
         case "log": {
-            return console.log(`${timestamp} ${grey(type.toUpperCase())} » ${content}`);
+            return console.log(`${timestamp} ${chalk.grey(type.toUpperCase())} » ${content}`);
         }
         case "rdy": {
-            return console.log(`${timestamp} ${green(type.toUpperCase())} » ${content}`);
+            return console.log(`${timestamp} ${chalk.blue(type.toUpperCase())} » ${content}`);
         }
         case "wrn": {
-            return console.log(`${timestamp} ${bgYellow(type.toUpperCase())} » ${content}`);
+            return console.log(`${timestamp} ${chalk.bgYellow(type.toUpperCase())} » ${content}`);
         }
         default: throw new TypeError("Invalid logger type.");
     }
 };
 
-exports.cmd = (...args) => this.log(...args, "cmd");
-exports.app = (...args) => this.log(...args, "app");
-exports.dbg = (...args) => this.log(...args, "dbg");
-exports.err = (...args) => this.log(...args, "err");
-exports.inf = (...args) => this.log(...args, "inf");
-exports.wrn = (...args) => this.log(...args, "wrn");
+export const cmd = (...args) => this.log(...args, "cmd");
+export const app = (...args) => this.log(...args, "app");
+export const dbg = (...args) => this.log(...args, "dbg");
+export const err = (...args) => this.log(...args, "err");
+export const inf = (...args) => this.log(...args, "inf");
+export const wrn = (...args) => this.log(...args, "wrn");
