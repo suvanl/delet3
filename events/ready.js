3ea811b0356d3395411bfce9858a209654477850
suvanl <32813609+suvanl@users.noreply.github.com>
2022-04-10 21:03:00 +0100

    Specify if running dev build in activity

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,10 @@
 const { green } = require("chalk");
+const ver = require("../package.json").version;
+const { NODE_ENV: env } = process.env;
 
 module.exports = async client => {
     client.logger.log(`${green(client.user.tag)} | ${green(client.users.cache.size - 1)} users | ${green(client.guilds.cache.size)} servers`, "rdy");
-    client.user.setActivity("#TeamSeas 🌊", { type: "WATCHING" });
+    client.user.setActivity(env.toLowerCase() == "production" ? `liftoff@v${ver} 🚀` : `[dev_build_${ver}]`, { type: "WATCHING" });
 
     if (process.argv.includes("--deploy")) await client.deploySlashCommand(client);
 };
