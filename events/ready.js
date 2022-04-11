c1a7a4e9788d8ec41b9b8682cf0cf400411e142e
suvanl <32813609+suvanl@users.noreply.github.com>
2022-04-11 20:58:08 +0100

    ready event: remove sq brackets from dev activity

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,7 @@ const { NODE_ENV: env } = process.env;
 
 export default async client => {
     client.logger.log(`${chalk.green(client.user.tag)} | ${chalk.green(client.users.cache.size - 1)} users | ${chalk.green(client.guilds.cache.size)} servers`, "rdy");
-    client.user.setActivity(env.toLowerCase() == "production" ? `liftoff@v${ver} 🚀` : `[dev_build_${ver}]`, { type: "WATCHING" });
+    client.user.setActivity(env.toLowerCase() == "production" ? `liftoff@v${ver} 🚀` : `dev_build_${ver}`, { type: "WATCHING" });
 
     if (process.argv.includes("--deploy")) await client.deploySlashCommand(client);
 };
