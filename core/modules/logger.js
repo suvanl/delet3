58dd8a78f1d17528eecee4c53cb8377072a4745d
suvanl <32813609+suvanl@users.noreply.github.com>
2022-04-15 20:21:51 +0100

    logger: change "log" colour to white To ensure the "LOG" category keyword is visible in all shells

--- core/modules/logger.js
+++ core/modules/logger.js
@@ -2,8 +2,8 @@ import chalk from "chalk";
 import { DateTime } from "luxon";
 
 export const log = (content, category = "log") => {
-    const offset = DateTime.now().toFormat("Z");  // narrow offset
-    const timestamp = `${DateTime.now().toFormat("y-MM-dd HH:mm:ss")} UTC${offset} |`;
+    const offset = DateTime.now().toFormat("Z");
+    const timestamp = `${DateTime.now().toFormat("y-MM-dd HH:mm:ss")} ${offset === "0" ? "" : `UTC${offset}`} |`;
 
     switch (category) {
         case "cmd": {
@@ -22,7 +22,7 @@ export const log = (content, category = "log") => {
             return console.log(`${timestamp} ${chalk.bgBlue(category.toUpperCase())} » ${content}`);
         }
         case "log": {
-            return console.log(`${timestamp} ${chalk.grey(category.toUpperCase())} » ${content}`);
+            return console.log(`${timestamp} ${chalk.white(category.toUpperCase())} » ${content}`);
         }
         case "ready": {
             return console.log(`${timestamp} ${chalk.green(category.toUpperCase())} » ${content}`);
