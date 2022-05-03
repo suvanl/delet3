feae518c15b35b3809ae257b418dec8d0fbaaafd
suvanl <32813609+suvanl@users.noreply.github.com>
2022-05-03 18:38:45 +0100

    Deprecate "ping" and "level" text commands

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,4 +1,6 @@
 export const run = async (client, message) => {
+    client.sendDeprecationWarning(message, "use the `/ping` slash command instead.");
+
     const msg = await message.channel.send("🏓 Pong!");
     msg.edit(`🏓 Pong! ${client.l10n(message, "latency")}: \`${msg.createdTimestamp - message.createdTimestamp}ms\`; WebSocket ping: \`${Math.round(client.ws.ping)}ms\`.`);
 };
