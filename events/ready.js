0886a2413a709424d0ba464af0b057aed916f2f6
suvanl <32813609+suvanl@users.noreply.github.com>
2022-07-28 22:04:36 +0100

    ready: set activity before logging "ready" msg

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,13 @@
 import chalk from "chalk";
 import pkg from "../package.json";
+import { ActivityType } from "discord.js";
 
 const { version: ver } = pkg;
 const { NODE_ENV: env } = process.env;
 
 export default async client => {
+    client.user.setActivity(env.toLowerCase() == "production" ? `liftoff@v${ver} 🚀` : `dev_build_${ver}`, { type: ActivityType.Watching });
     client.logger.log(`${chalk.green(client.user.tag)} | ${chalk.green(client.users.cache.size - 1)} users | ${chalk.green(client.guilds.cache.size)} servers`, "ready");
-    client.user.setActivity(env.toLowerCase() == "production" ? `liftoff@v${ver} 🚀` : `dev_build_${ver}`, { type: "WATCHING" });
 
     if (process.argv.includes("--deploy")) await client.deployApplicationCommand(client);
 };
