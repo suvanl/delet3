a3a889c5858687a75639b7f3f96c3f39e3a74aaa
suvanl <32813609+suvanl@users.noreply.github.com>
2022-08-01 17:28:13 +0100

    avatar command: ApplicationCommand-only

---
+++ interactions/commands/misc/avatar.js
@@ -0,0 +1,40 @@
+import { ApplicationCommandOptionType, EmbedBuilder } from "discord.js";
+import { stripIndents } from "common-tags";
+
+export const run = async (client, interaction) => {
+    const user = interaction.options.getUser("user");
+    const isServerSpecific = interaction.options.getBoolean("server");
+
+    const guildMember = interaction.guild.members.cache.get(user.id);
+    const size = 1024;
+    const url = isServerSpecific ? guildMember.displayAvatarURL({ size }) : user.displayAvatarURL({ size });
+
+    // Create and send embed
+    const embed = new EmbedBuilder()
+        .setColor("#99AAB5")
+        .setDescription(stripIndents`
+            🖼️ **${client.l10n(interaction, "avatar.user").replace(/%user%/g, user.tag)}**
+            🔗 **[${client.l10n(interaction, "avatar.url")}](${url})**`)
+        .setImage(url);
+    
+    interaction.reply({ embeds: [embed] });
+};
+
+export const data = {
+    name: "avatar",
+    description: "View a user's avatar in high resolution",
+    options: [{ 
+        name: "user",
+        type: ApplicationCommandOptionType.User,
+        description: "The user whose avatar you'd like to view",
+        required: true
+    },
+    { 
+        name: "server",
+        type: ApplicationCommandOptionType.Boolean,
+        description: "Whether you'd like to see the server-specific avatar of a Nitro user",
+        required: false
+    }]
+};
+
+export const global = true;
